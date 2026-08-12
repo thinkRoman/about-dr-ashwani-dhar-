484a3658eeaff4c97fc915397d94b1818d4cef4d
kooldhar <ashdhar@thinkroman.com>
2026-08-12 09:34:52 -0700

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-import { CNNHero } from "@/components/cnn-hero"
-import { VideoSpotlight, InstagramFeed } from "@/components/video-spotlight"
-import { VenturesGrid, QuickBio } from "@/components/ventures-grid"
-import { KairosHighlight } from "@/components/kairos-highlight"
-import { TrPharmaHighlight } from "@/components/trpharma-highlight"
-import { PharmaceuticalPortfolio } from "@/components/pharmaceutical-portfolio"
-import { CNNFooter } from "@/components/cnn-footer"
-import { Navigation } from "@/components/navigation"
+import ProfileRedesign from "@/components/profile-redesign"
 
 export default function AboutDrAshwaniDhar() {
-  return (
-    <main className="min-h-screen">
-      <Navigation />
-      <CNNHero />
-      <VideoSpotlight />
-      <QuickBio />
-      <KairosHighlight />
-      <TrPharmaHighlight />
-      <PharmaceuticalPortfolio />
-      <VenturesGrid />
-      <InstagramFeed />
-      <CNNFooter />
-    </main>
-  )
+  return <ProfileRedesign />
 }
